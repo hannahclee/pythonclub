96ed1032e69b8042b29ef4fcd80a64aa3432eeb4
hannahclee <33705455+hannahclee@users.noreply.github.com>
2019-02-18 20:32:39 -0800

    Assignment 5: Resource View

--- pythonclub/pythonclubapp/views.py
+++ pythonclub/pythonclubapp/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from .models import Resource
 
 # Create your views here.
 
 def index(request) :
     return render(request, 'pythonclubapp/index.html')
+
+def resources (request):
+    resource_list=Resource.objects.all()
+    return render (request, 'pythonclubapp/resources.html' , {'resource_list': resource_list})
