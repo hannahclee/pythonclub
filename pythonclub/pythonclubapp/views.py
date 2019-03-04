01e3387b9f84b717dd6612cd56de05d0ac8a4325
hannahclee <33705455+hannahclee@users.noreply.github.com>
2019-03-04 13:45:24 -0800

    Assignment 6: Meeting and Detail

--- pythonclub/pythonclubapp/views.py
+++ pythonclub/pythonclubapp/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Resource
+from .models import Meeting
 
 # Create your views here.
 
@@ -9,3 +10,12 @@ def index(request) :
 def resources (request):
     resource_list=Resource.objects.all()
     return render (request, 'pythonclubapp/resources.html' , {'resource_list': resource_list})
+
+def meetings (request):
+    meeting_list=Meeting.objects.all()
+    return render (request, 'pythonclubapp/meetings.html' , {'meeting_list': meeting_list})
+
+def meetingdetail (request, id): 
+    detail=get_object_or_404(Meeting, pk=id)
+    context = {'detail': detail}
+    return render (request, 'pythonclubapp/details.html' , context=context)
